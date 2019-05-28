bb6f2a784b4f171e485970441b13d733c3618269
Facundo Benitez <facundob91@gmail.com>
2019-05-28 19:52:58 -0300

    Updates

---
+++ precache-manifest.7a66ca7b3cfa300098d4d764e82afe0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7da606b591b8b92fd4b07dcb5b8776",
+    "url": "/pokedex-react/index.html"
+  },
+  {
+    "revision": "250b0bdfffc200e1c37d",
+    "url": "/pokedex-react/static/css/main.c70c989e.chunk.css"
+  },
+  {
+    "revision": "8a97fb0f9004c1c6a16d",
+    "url": "/pokedex-react/static/js/2.0a177643.chunk.js"
+  },
+  {
+    "revision": "250b0bdfffc200e1c37d",
+    "url": "/pokedex-react/static/js/main.36428b8d.chunk.js"
+  },
+  {
+    "revision": "01c750445d0b5a887b35",
+    "url": "/pokedex-react/static/js/runtime~main.ed006230.js"
+  },
+  {
+    "revision": "6a398990b1d7b6c11135bd5823555f99",
+    "url": "/pokedex-react/static/media/sprites.6a398990.png"
+  }
+]);
